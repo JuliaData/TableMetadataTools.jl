c28d195b58edcd4b7c3bc8ca60512bac851b682f
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-11-30 18:51:57 +0100

    fix toml2meta!

--- src/convenience.jl
+++ src/convenience.jl
@@ -48,8 +48,8 @@ See also: [`meta2toml`](@ref)
 function toml2meta!(table, toml::Union{AbstractString, IO})
     allmeta = TOML.parse(toml)
     allmeta["colmetadata"] = Dict(Symbol(k) => v for (k, v) in allmeta["colmetadata"])
-    dict2metadata!(table, allmeta["metadata"], style=allmeta["style"])
-    dict2colmetadata!(table, allmeta["colmetadata"], style=allmeta["style"])
+    dict2metadata!(table, allmeta["metadata"], style=Symbol(allmeta["style"]))
+    dict2colmetadata!(table, allmeta["colmetadata"], style=Symbol(allmeta["style"]))
     return table
 end
 
