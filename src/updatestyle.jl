00830446ec2277be22091303f5888d8c614dd527
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-12-03 22:12:52 +0100

    Add col kwarg to setcolmetadatastyle!

--- src/updatestyle.jl
+++ src/updatestyle.jl
@@ -23,20 +23,25 @@ setmetadatastyle!(table; style::Symbol=:note) =
     setmetadatastyle!(Returns(true), table, style=style)
 
 """
-    setcolmetadatastyle!([predicate], table; style::Symbol=:note)
+    setcolmetadatastyle!([predicate], table; style::Symbol=:note, [col::Symbol])
 
 Set style for column-level keys in `table` that match `predicate` to `style`.
 If `predicate` is omitted then all metadata is updated.
+If `col` is passed then it must be a column name and only metadata for this
+column is updated (if present).
 
 See also: [`setmetadatastyle`!](@ref), [`setallmetadatastyle!](@ref)
 """
-function setcolmetadatastyle!(predicate, table; style::Symbol=:note)
-    for (col, colmetakeys) in DataAPI.colmetadatakeys(table)
+function setcolmetadatastyle!(predicate, table; style::Symbol=:note,
+                              col::Union{Nothing, Symbol}=nothing)
+    for (c, colmetakeys) in DataAPI.colmetadatakeys(table)
+        # if col is passed skip all columns except col
+        isnothing(col) || c != col || continue
         for colmetakey in colmetakeys
             if predicate(colmetakey)
-                colmetavalue, colmetastyle = DataAPI.colmetadata(table, col, colmetakey, style=true)
+                colmetavalue, colmetastyle = DataAPI.colmetadata(table, c, colmetakey, style=true)
                 if colmetastyle != style
-                    DataAPI.colmetadata!(table, col, colmetakey, colmetavalue, style=style)
+                    DataAPI.colmetadata!(table, c, colmetakey, colmetavalue, style=style)
                 end
             end
         end
@@ -45,8 +50,9 @@ function setcolmetadatastyle!(predicate, table; style::Symbol=:note)
     return table
 end
 
-setcolmetadatastyle!(table; style::Symbol=:note) =
-    setcolmetadatastyle!(Returns(true), table, style=style)
+setcolmetadatastyle!(table; style::Symbol=:note,
+                     col::Union{Nothing, Symbol}=nothing) =
+    setcolmetadatastyle!(Returns(true), table, style=style, col=col)
 
 """
     setallmetadatastyle!([predicate], table; style::Symbol=:note)
