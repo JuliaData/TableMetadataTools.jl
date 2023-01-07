80e3de81ba1ef5d2eec7a8b10ddb4eb895d70c37
Bogumił Kamiński <bkamins@sgh.waw.pl>
2023-01-07 20:57:13 +0100

    add type piracy warning for unit method

--- src/standardkeys.jl
+++ src/standardkeys.jl
@@ -185,6 +185,12 @@ type of the column is `Units.Quantity` or `Dates.FixedPeriod` return unit of
 this type. If element type is `Number` return `Unitful.NoUnits`. Union with
 `Missing` is allowed in the above cases. For all other cases return `missing`.
 
+Warning! The proposed definition of `unit` is type piracy, but it is made
+for user convenience. It works under the assumption that other methods for
+`unit` (defined in other packages) always take one positional argument.
+If this assumption would not hold in the future the definition of `unit` in
+this package might change.
+
 See also: [`unit!`](@ref), [`units`](@ref)
 ```
 """
